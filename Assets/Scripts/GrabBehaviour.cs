a8ac6ebbd740de4a05da857f4e4e78c931258e35
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2021-04-19 23:33:09 +0200

    If you can grab an object your color will go green

--- Assets/Scripts/GrabBehaviour.cs
+++ Assets/Scripts/GrabBehaviour.cs
@@ -8,9 +8,19 @@ public class GrabBehaviour : MonoBehaviour
     private bool _canGrab = false;
     public Transform grabPosition;
     private GameObject _lastPersonTouched;
+    private SpriteRenderer _spriteRenderer;
 
+    public Color canGrabColor;
+    public Color normalColor;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
     private void Update()
     {
+        _spriteRenderer.color = (_canGrab) ? canGrabColor : normalColor;
         if (_canGrab && Input.GetKey(KeyCode.Space))
         {
             _lastPersonTouched.transform.position = grabPosition.position;
