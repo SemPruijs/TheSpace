9af65baa278efc5f7a363e5e99c894971ac12447
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2021-04-19 22:47:46 +0200

    Add smooth camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject target;
+    public Vector3 offset;
+    public float trackingDelay;
+
+    private void LateUpdate()
+    {
+        var desiredPosition = target.transform.position + offset;
+        var smoothedPosition = Vector3.Lerp(gameObject.transform.position, desiredPosition, trackingDelay * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
