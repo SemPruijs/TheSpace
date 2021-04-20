c1b6f5868b812dc4535e05d0f0535da0cde811f7
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2021-04-20 22:59:43 +0200

    Add GameManager.cs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Makes GameManager singleton
+    private static GameManager _instance;
+    public static GameManager Instance {
+        get {
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+        }
+    }
+
+    public enum State
+    {
+        Menu,
+        InGame,
+        Dead
+    }
+
+    public State state;
+
+
+    public void Menu()
+    {
+        state = State.Menu;
+    }
+
+    public void InGame()
+    {
+        state = State.InGame;
+    }
+
+    public void Dead()
+    {
+        state = State.Dead;
+    }
+}
