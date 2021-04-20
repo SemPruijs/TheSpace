0f1f9a5a0cb3b2ce2a4887c0674abce5fd08f541
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2021-04-20 17:07:10 +0200

    Make SetGrabPosition() function

--- Assets/Scripts/GrabBehaviour.cs
+++ Assets/Scripts/GrabBehaviour.cs
@@ -9,13 +9,20 @@ public class GrabBehaviour : MonoBehaviour
     private bool _isGrabbing;
     public Transform grabPosition;
     private GameObject _lastPersonTouched;
+    private Vector3 _startGrabPosition;
 
+
+    private void Start()
+    {
+        _startGrabPosition = grabPosition.localPosition;
+    }
 
     private void Update()
     {
         if (_canGrab && Input.GetKey(KeyCode.Space))
         {
             _lastPersonTouched.transform.position = grabPosition.position;
+            SetGrabPosition();
         }
 
         _isGrabbing = Input.GetKey(KeyCode.Space);
@@ -38,6 +45,18 @@ public class GrabBehaviour : MonoBehaviour
         if (other.gameObject.CompareTag("Person") && !_isGrabbing)
         {
             _canGrab = false;
+        }
+    }
+
+    private void SetGrabPosition()
+    {
+        if ((int) Input.GetAxisRaw("Horizontal") == -1)
+        {
+            grabPosition.localPosition = new Vector3(_startGrabPosition.x * -1f, grabPosition.localPosition.y,
+                grabPosition.localPosition.z);
+        } else if ((int) Input.GetAxisRaw("Horizontal") == 1)
+        {
+            grabPosition.localPosition = _startGrabPosition;
         }
     }
 }
