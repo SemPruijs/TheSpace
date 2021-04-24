90817d2d1f0af234e85617d5ae45d0acfe2c1657
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2021-04-24 13:01:52 +0200

    Implement help i am lost text field

---
+++ Assets/Scripts/LostInFieldTextField.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LostInFieldTextField : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gameObject.GetComponent<Animator>().SetBool("ShowField", true);
+        }
+    }
+}
