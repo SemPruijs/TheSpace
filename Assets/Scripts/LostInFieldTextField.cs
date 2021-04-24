94ff221579569a1aae1c64856e730a58940c9519
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2021-04-24 16:55:24 +0200

    Add please help me sound

--- Assets/Scripts/LostInFieldTextField.cs
+++ Assets/Scripts/LostInFieldTextField.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 
 public class LostInFieldTextField : MonoBehaviour
 {
+    public AudioClip helpMeClip;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             gameObject.GetComponent<Animator>().SetBool("ShowField", true);
+            GetComponent<AudioSource>().PlayOneShot(helpMeClip);
         }
     }
 }
