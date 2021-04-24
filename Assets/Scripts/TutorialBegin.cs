4c1a1b3bf92298c1d19eb6ab43280a007b69757f
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2021-04-24 14:54:21 +0200

    Add tutorial

---
+++ Assets/Scripts/TutorialBegin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialBegin : MonoBehaviour
+{
+    public GameObject player;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Sword"))
+        {
+            player.transform.position = GameManager.Instance.spawnPoint.position;
+            GameManager.Instance.cameraFollow.gameObject.transform.position = GameManager.Instance.cameraFollow.offset;
+        }
+    }
+}
